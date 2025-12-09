74f33b32b0922d06bdc00947890a371744cc0ddf
CePrhassani <programhasani@gmail.com>
2025-12-09 18:25:22 +0330

    add Step-02-with-OOD to project

---
+++ Step-02-with-OOD/services/SmsSender.java
@@ -0,0 +1,10 @@
+package services;
+
+class SmsSender implements MessageSender{
+    public void sendEmail(String to, String message){
+        
+    }
+    public void sendSMS (String to, String message){
+        System.out.println("Sending SMS to " + to + ": " + message);
+    }
+}
