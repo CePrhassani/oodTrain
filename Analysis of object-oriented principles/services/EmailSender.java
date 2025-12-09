cc71ec899b4b7916ed12dd023a3afb2e532d350e
Sepehr Noroozi <noroozi.sepehr@gmail.com>
2025-12-09 20:52:22 +0330

    docs: OOD analysis part 1 (Sepehr Noroozi Chakoli: SRP, OCP, LSP, ISP)

---
+++ Analysis of object-oriented principles/services/EmailSender.java
@@ -0,0 +1,10 @@
+package services;
+
+class EmailSender implements MessageSender{
+    public void sendEmail(String to, String message){
+        System.out.println("Sending email to " + to + ": " + message);
+    }
+    public void sendSMS (String to, String message){
+        
+    }
+}
